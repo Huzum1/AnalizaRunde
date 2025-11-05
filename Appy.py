4a1d3e1fc7199f3bff640f5f0f377784fd11f128
Huzum1 <31430481+Huzum1@users.noreply.github.com>
2025-11-05 03:33:26 +0100

    Update Appy.py

--- Appy.py
+++ Appy.py
@@ -716,7 +716,7 @@ with tab_analiza:
         toate_variantele_metrici.sort(key=lambda x: x['scor'], reverse=True)
         
         # Selectare număr de variante de afișat
-        nr_top = st.slider("Număr variante top:", 10, 1000, 100, 10)
+        nr_top = st.slider("Număr variante top:", 10, 1000, 100, 10, 1155)
         
         top_variante = toate_variantele_metrici[:nr_top]
         
